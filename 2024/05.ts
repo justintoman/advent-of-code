86c05d7fa65b7aa7eebb4653bd4ba98f94cdbfa8
Justin Toman <justintoman@gmail.com>
2024-12-06 16:57:23 -0600

    Day 5 stub

---
+++ 2024/05.ts
@@ -0,0 +1,12 @@
+import { day } from "./2024";
+
+day(5, (input) => {
+  return {
+    part1() {
+      return "";
+    },
+    part2() {
+      return "";
+    },
+  };
+});
--- 2024/index.ts
+++ 2024/index.ts
@@ -2,5 +2,6 @@ import "./01";
 import "./02";
 import "./03";
 import "./04";
+import "./05";
 
 export { run } from "./2024";
