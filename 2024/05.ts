0808eaa2ea06f4b4d84e53579c27189de2ad18f9
Justin Toman <justintoman@gmail.com>
2024-12-06 17:24:28 -0600

    Day 5 part 1

--- 2024/05.ts
+++ 2024/05.ts
@@ -1,9 +1,48 @@
+import chalk from "chalk";
 import { day } from "./2024";
 
 day(5, (input) => {
+  const lines = input.split(/\r?\n/);
+  const rules = lines
+    .filter((line) => line.includes("|"))
+    .map((line) => line.split("|").map((num) => Number(num)));
+  const updates = lines
+    .filter((line) => line.includes(","))
+    .map((line) => line.split(",").map((num) => Number(num)));
   return {
     part1() {
-      return "";
+      let sum = 0;
+      updates.forEach((update, i) => {
+        const applicableRules = rules.filter((rule) =>
+          rule.every((num) => update.includes(num))
+        );
+        // console.log(
+        //   `Update ${i} rules: ${applicableRules.map((rule) => rule.join("|"))}`
+        // );
+        if (
+          applicableRules.every(([before, after]) => {
+            const valid = update.indexOf(before) < update.indexOf(after);
+            if (!valid) {
+              // console.log(
+              //   `Update ${i}: ${
+              //     valid ? "valid" : "invalid"
+              //   }-${before} must come before ${after} in ${update}`
+              // );
+            }
+            return valid;
+          })
+        ) {
+          const itoAdd = Math.round(update.length / 2) - 1;
+          const inc = update[itoAdd];
+          // console.log(
+          //   `${i} is valid: len=${update.length}, mid=${itoAdd}, ${update.map(
+          //     (num, ix) => (ix === itoAdd ? chalk.bold(num) : chalk.gray(num))
+          //   )} | adding ${inc}`
+          // );
+          sum += inc;
+        }
+      });
+      return sum;
     },
     part2() {
       return "";
